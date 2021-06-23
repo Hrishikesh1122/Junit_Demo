cb8c124edcb92422b6008de69dcdbc42b62e417a
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-06-23 14:24:47 +0530

    Added : Lambda Expressions

---
+++ UserRegistration/src/main/java/service/Imessage.java
@@ -0,0 +1,6 @@
+package service;
+
+@FunctionalInterface
+public interface Imessage {
+	public abstract void displayError();
+}
--- UserRegistration/src/main/java/service/Registration.java
+++ UserRegistration/src/main/java/service/Registration.java
@@ -11,13 +11,19 @@ import java.util.regex.Pattern;
 
 public class Registration {
 	
+	
 	/**
 	 * Contains regex pattern to test Firstname
 	 * @param firstName
 	 * @return True if matches regex pattern orterwise false
 	 */
 	public  boolean getFirstName(String firstName) {
-		return Pattern.matches("[A-Z]+[a-z]{2,}", firstName);
+		boolean check;
+		check = Pattern.matches("[A-Z]+[a-z]{2,}", firstName);
+		if(!check) {
+			displayErrorMessage("Invalid first name");
+		}
+		return check;
 	}
 	
 	/**
@@ -26,7 +32,12 @@ public class Registration {
 	 * @return True if matches regex pattern orterwise false
 	 */
 	public boolean getLastName(String lastName) {
-		return Pattern.matches("[A-Z]+[a-z]{2,}", lastName);
+		boolean check;
+		check= Pattern.matches("[A-Z]+[a-z]{2,}", lastName);
+		if(!check) {
+			displayErrorMessage("Invalid last name");
+		}
+		return check;
 	}
 	
 	/**
@@ -35,7 +46,12 @@ public class Registration {
 	 * @return True if matches regex pattern orterwise false
 	 */
 	public boolean getPhoneNumber(String phone) {
-		return Pattern.matches("^[1-9][0-9]\s[1-9]{10}$", phone);
+		boolean check;
+		check = Pattern.matches("^[1-9][0-9]\s[1-9]{10}$", phone);
+		if(!check) {
+			displayErrorMessage("Invalid phone number");
+		}
+		return check;
 	}
 	
 	/**
@@ -44,7 +60,13 @@ public class Registration {
 	 * @return True if matches regex pattern otherwise false
 	 */
 	public boolean getEmail(String email) {
-		return Pattern.matches("^[a-z0-9.+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", email);	
+		boolean check;
+		check = Pattern.matches("^[a-z0-9.+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", email);
+		if(!check) {
+			displayErrorMessage("Invalid email");
+		}
+		return check;
+		
 	}
 	
 	/**
@@ -53,7 +75,27 @@ public class Registration {
 	 * @return True if matches regex pattern orterwise false
 	 */
 	public boolean getPassword(String password) {
-		return Pattern.matches("^(?=.*[A-Z])(?=.*\\d)(?=.*[a-zA-z0-9!@#$%^&*()_+=-{};':<>,./?~`]).{8,}$", password);	
+		boolean check;
+		check = Pattern.matches("^(?=.*[A-Z])(?=.*\\d)(?=.*[a-zA-z0-9!@#$%^&*()_+=-{};':<>,./?~`]).{8,}$", password);	
+		if(!check) {
+			displayErrorMessage("Invalid password");
+		}
+		return check;
+		
+	}
+	
+	/**
+	 * Displays error message using lembda expression
+	 * @param message (Error message from the method from which it is called)
+	 */
+	public void displayErrorMessage(String message) {
+		Imessage i=()-> System.out.println(message);
+		i.displayError();
+	}
+	public static void main(String[] args) {
+		Registration r = new Registration();
+		r.getFirstName("h987i"); //Shows invalid First name message
+		r.getFirstName("Hrishi"); //Does not show error message 
 	}
 
 }
